fc48f4ece644b75d07654e2b6a5d75f580cb134e
Marumasa <77374813+malken21@users.noreply.github.com>
2024-11-24 15:31:54 +0900

    Update background.js

--- background.js
+++ background.js
@@ -1,13 +1,17 @@
 chrome.tabs.onUpdated.addListener(() => {
     chrome.tabs.query({ currentWindow: true }, (tabs) => {
-        const activeTabId = tabs.find(tab => tab.active).id;
+        const activeTab = tabs.find(tab => tab.active);
+
+        if (!activeTab) return; // 現在のタブが取得できなかったら return
+
+        const activeTabId = activeTab.id;
 
         tabs.forEach(tab => {
-            if (tab.id !== activeTabId) {
-                chrome.tabs.remove(tab.id).catch(error => {
-                    console.log(`Error removing tab ${tab.id}: ${error}`);
-                });
-            }
+            if (tab.id === activeTabId) return; // 現在のタブだったら return
+
+            chrome.tabs.remove(tab.id).catch(error => {
+                console.log(`Error removing tab ${tab.id}: ${error}`);
+            });
         });
     });
 });
